eccd2496d1805732b01a85157d30910654192019
educanjr <eduardo.canaljunior@gmail.com>
2025-03-15 17:09:23 -0300

    Adding Result Object

---
+++ src/ConwayGameOfLife.Application/Common/ResultObject.TResult.cs
@@ -0,0 +1,46 @@
+﻿namespace ConwayGameOfLife.Application.Common;
+
+public class ResultObject<TResult> : ResultObject
+{
+    public ResultObject()
+    {
+
+    }
+    private ResultObject(TResult result)
+    {
+        Result = result;
+        IsSuccess = true;
+    }
+
+    private ResultObject(ResultError errorResult)
+        : base(errorResult)
+    {
+    }
+
+    public TResult? Result { get; protected set; }
+
+    public static new ResultObject<TResult> Error(params string[] errorMessages) => new ResultObject<TResult>(new ResultError
+    {
+        Code = ErrorCode.InternalError,
+        Message = string.Join(Environment.NewLine, errorMessages)
+    });
+
+    public static new ResultObject<TResult> NotFound(params string[] errorMessages) => new ResultObject<TResult>(new ResultError
+    {
+        Code = ErrorCode.NotFound,
+        Message = string.Join(Environment.NewLine, errorMessages)
+    });
+
+    public static ResultObject<TResult> Success(TResult result) => new(result);
+
+
+    public static implicit operator TResult?(ResultObject<TResult> result)
+    {
+        return result == null ? default : result.Result;
+    }
+
+    public static implicit operator ResultObject<TResult>(TResult result)
+    {
+        return new ResultObject<TResult>(result);
+    }
+}
---
+++ src/ConwayGameOfLife.Application/Common/ResultObject.cs
@@ -0,0 +1,53 @@
+﻿namespace ConwayGameOfLife.Application.Common;
+
+public class ResultObject
+{
+    protected ResultObject()
+    {
+        IsSuccess = false;
+    }
+
+    private ResultObject(bool succeeded)
+        : this()
+    {
+        IsSuccess = succeeded;
+    }
+
+    protected ResultObject(ResultError errorResult)
+        : this()
+    {
+        IsSuccess = false;
+        ErrorResult = errorResult;
+    }
+
+    public bool IsSuccess { get; protected init; }
+
+    public ResultError? ErrorResult { get; init; }
+
+
+    public static implicit operator bool(ResultObject value)
+    {
+        return value.IsSuccess;
+    }
+
+    public static implicit operator ResultObject(bool value)
+    {
+        return new ResultObject(value);
+    }
+
+    public static ResultObject Success() => new(true);
+
+    public static ResultObject Error(params string[] errorMessages) => new(
+        new ResultError
+        {
+            Code = ErrorCode.InternalError,
+            Message = string.Join(Environment.NewLine, errorMessages)
+        });
+
+    public static ResultObject NotFound(params string[] errorMessages) => new(
+        new ResultError
+        {
+            Code = ErrorCode.NotFound,
+            Message = string.Join(Environment.NewLine, errorMessages)
+        });
+}
